5372541a68455965966d56ad4ee41c46d932e4da
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-06-11 00:43:23 +0500

    Login in Progress

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,104 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import styled from 'styled-components'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { FaLock } from 'react-icons/fa'
 
 const Login = () => {
+
+  const [emailInput, setEmail] = useState(false)
+  const [passwordInput, setPassword] = useState(false)
+
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  // console.log(emailInput)  
+  
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <Main>
+      <div className="login">
+        <p>Log in you Account</p>
+        {!emailInput &&
+          <>
+            <div className="input-field">
+              <span className='icon'><BsFillPersonFill /></span>
+              <input ref={emailRef} id='username' className='input-box' placeholder='Username or Email' type="text" />
+            </div>
+            <button className='btn' onClick={() => setEmail(emailRef.current.value)}>Continue with Email</button>
+          </>
+        }
+        {emailInput && <h1>{emailInput}</h1>}
+        {!passwordInput &&
+          <>
+            <div className="input-field">
+              <span className='icon'><FaLock /></span>
+              <input ref={passwordRef} id='password' className='input-box' placeholder='Enter your Password' type="password" />
+            </div>
+            <div className="otherChecks" style={{marginTop: '5px'}}>
+              <input type='checkbox' id='rememberMe' value="" />
+              <label htmlFor='rememberMe' style={{marginLeft: '5px'}}>Keep me logged in</label>
+            </div>
+            <button className='btn' onClick={() => { setPassword(passwordRef.current.value) }}>Login</button>
+          </>
+        }
+        {passwordInput && <h1>{passwordInput}</h1>}
+        </div>
+    </Main>
   )
 }
 
 export default Login
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  /* text-align: center; */
+  .login{
+    border: 0.1rem solid #bdb5b5;
+    border-radius: 3px;
+    padding: 1rem 8rem;
+    height: 60vh;
+    p{
+      font-size: 25px;
+      font-weight: 600;
+      text-align: center;
+      margin-bottom: 1.5rem;
+    }
+    .input-field{
+      display: flex;
+      border: 0.1rem solid #d6d4d4;
+      border-radius: 4px;
+      width: 335px;
+      height: 40px;
+      :focus-within, :hover{
+        border-color: #67b9c7;
+      }
+      .icon{
+        margin: auto 10px;
+        font-size: 18px;
+        color: #5E6D55;
+      }
+      .input-box{
+        font-size: 16px;
+        border: none;
+        width: 100%;
+      }
+    }
+    .btn{
+      margin: 1.5rem 0;
+      width: 100%;
+      border: none;
+      background-color: #05ac05;
+      font-size: 17px;
+      padding: 14px 0;
+      border-radius: 2rem;
+      color: white;
+      cursor: pointer;
+      :hover{
+        background-color: green;
+      }
+    }
+  } 
+`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,36 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-    <Container>
-        <ul>
-            <a><li><Link to="/">Home</Link></li></a>
-            <a><li><Link to="/cart">Cart</Link></li></a>
-            <a><li><Link to="/products">Products</Link></li></a>
-            <a><li><Link to="/login">Login</Link></li></a>
-            <a><li><Link to="/dropdown">Dropdown Header</Link></li></a>
-        </ul>
-    </Container>
+    <Ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/cart">Cart</Link></li>
+      <li><Link to="/products">Products</Link></li>
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/dropdown">Dropdown Header</Link></li>
+    </Ul>
   )
 }
 
 export default Navbar
 
-const Container = styled.div`
-  ul{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #000000;
-    font-size: 25px;
-    height: 10vh;
-    text-transform: uppercase;
+const Ul = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #000000;
+  font-size: 20px;
+  height: 10vh;
+  text-transform: uppercase;
+  li{
+    margin: 0 20px;
     a{
       color: #23b0df;
       :hover{
-        color: #0390be;
-      }
-    }
-    li{
-      margin: 0 20px;
-      :hover{
         border-bottom: .001rem solid #005f96;
+        color: #0390be;
       }
     }
   }
